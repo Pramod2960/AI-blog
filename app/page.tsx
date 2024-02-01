c05dbc711249ce7b6895f51df9d239638a22c27e
pramod <pramodkumar2960.com>
2024-02-01 13:33:27 +0530

    hh

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,8 @@ export default function Home() {
   return (
    <div>
     <div className="container"></div>
-    <h1 className="text-red">Hello</h1>
-    <h2 className="font-bold 2xl">Word</h2>
-
+    <h1 className="text-red">AI - Blog </h1>
+    <h2 className="font-bold 2xl">Website</h2>
    </div>
   );
 }
